74d13bc76200eb911393ea1bd903b89547e42599
Tadeo <tadeo0448@outlook.com>
2024-10-17 07:57:41 -0600

    Commit1

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/home_page.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: const HomePage(),
+        theme: ThemeData(
+            tabBarTheme: const TabBarTheme(indicatorColor: Colors.pink)));
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import '../utils/my_tab.dart';
+
+//Importar 5 tabs
+import '../tab/burger_tab.dart';
+import '../tab/donut_tab.dart';
+import '../tab/pancakes_tab.dart';
+import '../tab/pizza_tab.dart';
+import '../tab/smoothie_tab.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    //Donut tab
+    const MyTab(
+      iconPath: "lib/icons/donut.png",
+    ),
+
+    //Donut tab
+    const MyTab(
+      iconPath: "lib/icons/burger.png",
+    ),
+
+    //smoothie tab
+    const MyTab(
+      iconPath: "lib/icons/smoothie.png",
+    ),
+
+    //pancake tab
+    const MyTab(
+      iconPath: "lib/icons/pancakes.png",
+    ),
+
+    //pizza tab
+    const MyTab(
+      iconPath: "lib/icons/pizza.png",
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey[800],
+            ),
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 15.0),
+              child: Icon(Icons.person),
+            )
+          ], //Crea un area de gestion de cuentas
+        ),
+        body: Column(
+          children: [
+            //text "I want to eat"
+            const Padding(
+              padding: EdgeInsets.all(24.0),
+              child: Row(
+                children: [
+                  Text(
+                    "I want To ",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Text(
+                    "Eat",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  ),
+                ],
+              ),
+            ),
+
+            //Tap bar
+            TabBar(tabs: myTabs),
+
+            //Tab bar view
+            const Expanded(
+                child: TabBarView(children: [
+              DonutTab(),
+              BurgerTab(),
+              SmoothieTab(),
+              PizzaTab(),
+              PancakesTab()
+            ]))
+
+            //Total del carrito
+          ],
+        ),
+      ),
+    );
+  }
+}
